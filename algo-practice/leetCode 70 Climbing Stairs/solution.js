b334fe3ac5186ecda3888d840e4ff48e4ec2df76
Quintin Russell <quinn.j.russell@gmail.com>
2022-02-07 19:05:12 +0000

    leetCode 70 solution completed

--- algo-practice/leetCode 70 Climbing Stairs/solution.js
+++ algo-practice/leetCode 70 Climbing Stairs/solution.js
@@ -5,6 +5,20 @@
 
 // combinations using recursion
 
-var climbStairs = function (n) {
+  var climbStairs = function (n) {
+    var res = Array(n + 1).fill(0);
+    var temp = 0;
+    var m = 2;
+    res[0] = 1;
 
-};
+    for (var i = 1; i <= n; i++) {
+      var s = i - m - 1;
+      var e = i - 1;
+      if (s >= 0) {
+        temp -= res[s];
+      }
+      temp += res[e];
+      res[i] = temp;
+    }
+    return res[n];
+  };
