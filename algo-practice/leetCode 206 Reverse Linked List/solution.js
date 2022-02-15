49589df3832d30703623432d74f2e6937e240c41
Quintin Russell <quinn.j.russell@gmail.com>
2022-02-14 18:49:11 -0800

    added solution to reversing a linked list

--- algo-practice/leetCode 206 Reverse Linked List/solution.js
+++ algo-practice/leetCode 206 Reverse Linked List/solution.js
@@ -10,5 +10,15 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
+ let prev = null;
+ let next = null;
 
+ while(head !== null) {
+   next = head.next;
+   head.next = prev;
+   prev = head;
+   head = next
+ }
+
+ return prev
 };
