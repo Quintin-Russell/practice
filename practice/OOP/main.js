8e54a1f09b787752b81c086c09207a6d25dc031c
Quintin Russell <quinn.j.russell@gmail.com>
2022-05-02 16:30:41 -0700

    added Shape and calcPerimeter

--- practice/OOP/main.js
+++ practice/OOP/main.js
@@ -1,7 +1,15 @@
 class Shape {
-
-  name;
-  sides;
-  sideLength;
-
+constructor(name, sides, sideLength) {
+  this.name = name;
+  this.sides = sides;
+  this.sideLength = sideLength;
 }
+}
+
+Shape.prototype.calcPerimeter = function () {
+  return this.sides * this.sideLength
+}
+
+const shape = new Shape('square', 4, 10)
+
+console.log('calcPerimerter:', shape.calcPerimeter())
