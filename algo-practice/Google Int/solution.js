6049fc9346c8d52d76cd21188282878d14ee5ca0
Quintin Russell <quinn.j.russell@gmail.com>
2022-06-14 22:09:47 -0700

    added solution file

---
+++ algo-practice/Google Int/solution.js
@@ -0,0 +1,19 @@
+/*
+str_a: "aaaabbc"
+str_b: "aacbbabc"
+
+a a a a b b c
+i
+
+a a c  b  b a b c
+j
+
+chars = {
+
+}
+
+subproblem:
+  is strb[j] in chars
+    is chars[strb[j]] the end char of stra
+    j++
+*/
